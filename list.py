aef376aa2866723c0923060aba66aa900e34b2af
erp <sonu@tecereceptives.com>
2020-01-07 13:34:00 +0530

    Hello this is list

---
+++ list.py
@@ -0,0 +1,7 @@
+l=int(input("Emter a Number:- "))
+if l<=0:
+    print("Enter a positive number")
+elif l%2==0:
+    print("Enter number ",l," is even ")
+else:
+    print("Enter number ",l," is odd")
